1bf1bc98d0cb2aa30f01f1214b8c2e4690dbe0a3
jonatan javier garzon nieto <jonatangarzon@colegioguanenta.edu.co>
2026-05-06 07:39:07 -0500

    Add files via upload

--- structura_basica.py
+++ structura_basica.py
@@ -8,31 +8,35 @@ pygame.init()
 ventana = pygame.display.set_mode((400,400))
 
 # establecer titulo de la ventana
-pygame.display.set_caption("pygame Guanenta")
+pygame.display.set_caption("pygame Jonatan")
 
 # definir colores
 Azul = (0,0,255)
 Rojo = (255,0,0)
 verde = (0,255,0)
-verde_oscuro = (71,98,0)
+amarillo = (255,255,0)
+gris = (128,128,128)
 
 # creamos una superficie
 superficie_1 = pygame.Surface((200,200))
 superficie_2 = pygame.Surface((200,200))
 superficie_3 = pygame.Surface((200,200))
 superficie_4 = pygame.Surface((200,200))
+superficie_5 = pygame.Surface((200,200))
 
 # Rellenamos la superficie de color
 superficie_1.fill(Azul)
 superficie_2.fill(Rojo)
 superficie_3.fill(verde)
-superficie_4.fill(verde_oscuro)
+superficie_4.fill(amarillo)
+superficie_5.fill(gris)
 
 # agregar o mover la ventana
 ventana.blit(superficie_1, (0,0))
-ventana.blit(superficie_2, (200,200))
+ventana.blit(superficie_2, (200,0))
 ventana.blit(superficie_3, (0,200))
-ventana.blit(superficie_4, (300,0))
+ventana.blit(superficie_4, (200,200))
+ventana.blit(superficie_5, (100,100))
 
 # actualisar visualisacion de la ventana
 pygame.display.flip()
