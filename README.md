8b12c6637ea14effdb8406b0b84b10d7c36b4c9d
jonatan javier garzon nieto <jonatangarzon@colegioguanenta.edu.co>
2026-05-07 18:29:14 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # intro-pygame
 structura básica de u juego 2d usando la Liberia  de pygame 
+![alt text](9448c8254e1307477fed0b3ec0f2b9e4.jpg)
--- ciudad_pacman.py
+++ ciudad_pacman.py
@@ -1,15 +1,42 @@
 # Crear una ciudad de hierro o parque de atraaciones usando los elementos graficos vistos con pygame (lineas, rectangulos, cuadrados, poligonos, circulos, elipses, arcos y textos) en donde los personajes son pacmanans
 
 
+# importamos la libreria pygame
 import pygame
 import sys
-# establecer dimensiones de la ventana (proporción sugerida para esta bandera)
-ventana = pygame.display.set_mode((450, 270))
+import math
 
-# inicializamos los modulos de la libreria
+
+# inicializamos los modulos de la librería
 pygame.init()
 
+# Establecer dimensiones de la ventana
+ventana = pygame.display.set_mode((1000,700))
 
-# establecer el titulo de la ventana
-pygame.display.set_caption("ciudad de pacman")
+# establecer titulo de la ventana
+pygame.display.set_caption("Dibujar formas básicas")
 
+# definición colores
+rojo = (255,0,0)
+azul = (0,0,255)
+naranja = (255,165,0)
+verde = (0,255,0)
+rosado = (255,192,203)
+amarillo = (255,255,0)
+blanco = (255,255,255)
+cian = (0,255,255)
+
+
+# bucle principal del juego
+while True:
+
+    for event in pygame.event.get():
+        # Al hacer click sobre el boton de cerrar la ventana el juego termina
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+    # rectangulo
+    pygame.draw.rect(ventana, azul, (150,150,50,50))
+
+    # actualizar visualización de la ventana
+    pygame.display.flip()
