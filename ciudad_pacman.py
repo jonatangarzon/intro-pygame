91be59a2060226a643cc0102882b8627cc36f7ba
jonatan javier garzon nieto <jonatangarzon@colegioguanenta.edu.co>
2026-05-07 18:04:44 -0500

    Add files via upload

---
+++ ciudad_pacman.py
@@ -0,0 +1,15 @@
+# Crear una ciudad de hierro o parque de atraaciones usando los elementos graficos vistos con pygame (lineas, rectangulos, cuadrados, poligonos, circulos, elipses, arcos y textos) en donde los personajes son pacmanans
+
+
+import pygame
+import sys
+# establecer dimensiones de la ventana (proporción sugerida para esta bandera)
+ventana = pygame.display.set_mode((450, 270))
+
+# inicializamos los modulos de la libreria
+pygame.init()
+
+
+# establecer el titulo de la ventana
+pygame.display.set_caption("ciudad de pacman")
+
